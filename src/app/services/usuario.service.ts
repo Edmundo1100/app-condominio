93a8559b5e9ae6a557dc9322c27774d1e79d2438
Edmundo <edmundo1100@hotmail.com>
2023-02-26 14:35:11 -0300

    limpeza

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -25,68 +25,44 @@ export class UsuarioService {
           localStorage.setItem('usuario', window.btoa(JSON.stringify(usuario)));
           this.router.navigate(['']);
       }));
-
-    // return this.mockUsuarioLogin(usuario).pipe(tap((resposta) => {
-    //   if (!resposta.sucesso) return;
-
-    //   localStorage.setItem('token', window.btoa(JSON.stringify("TokenQueSeriaGeradoPelaAPI")));
-    //   localStorage.setItem('usuario', window.btoa(JSON.stringify(usuario)));
-    //   this.router.navigate(['']);
-    // }));
   }
 
-  private mockUsuarioLogin(usuario: IUsuario): Observable<any> {
-    var retornoMock: any = [];
+  // deslogar() {
+  //   localStorage.clear();
+  //   this.router.navigate(['login']);
+  // }
 
-    if (usuario.usuario === "edmundo" && usuario.senha == "123") {
-      retornoMock.sucesso = true;
-      retornoMock.usuario = usuario;
-      retornoMock.token = "TokenQueSeriaGeradoPelaAPI";
-      return of(retornoMock);
-    }
-
-    retornoMock.sucesso = false;
-    retornoMock.usuario = usuario;
-    return of(retornoMock);
-  }
-
-  deslogar() {
-    localStorage.clear();
-    this.router.navigate(['login']);
-  }
-
-  obterUsuarioLogado() {
-    const usuario = localStorage.getItem('usuario');
-    if (usuario) {
-      const decodedUsuario = JSON.parse(window.atob(usuario));
-      return decodedUsuario;
-    } else {
-      return null;
-    }
-  }
+  // obterUsuarioLogado() {
+  //   const usuario = localStorage.getItem('usuario');
+  //   if (usuario) {
+  //     const decodedUsuario = JSON.parse(window.atob(usuario));
+  //     return decodedUsuario;
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
 
-  get obterIdUsuarioLogado() {
-    const usuario = localStorage.getItem('usuario')
-    if (usuario) {
-      return (JSON.parse(window.atob(usuario)) as IUsuario).id;
-    } else {
-      return null;
-    }
+  // get obterIdUsuarioLogado() {
+  //   const usuario = localStorage.getItem('usuario')
+  //   if (usuario) {
+  //     return (JSON.parse(window.atob(usuario)) as IUsuario).id;
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
-  }
+  // get obterTokenUsuario(): string | null {
+  //   const token = localStorage.getItem('token')
+  //   if (token) {
+  //     return JSON.parse(window.atob(token))
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
-  get obterTokenUsuario(): string | null {
-    const token = localStorage.getItem('token')
-    if (token) {
-      return JSON.parse(window.atob(token))
-    } else {
-      return null;
-    }
-  }
-
-  get logado(): boolean {
-    return localStorage.getItem('token') ? true : false;
-  }
+  // get logado(): boolean {
+  //   return localStorage.getItem('token') ? true : false;
+  // }
 }
 
